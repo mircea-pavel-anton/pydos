b3588110c69623089f90c893fa781eb83fb8f062
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-16 14:30:55 +0200

    Get the bit array from cli params

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from decode import decode
 from encode import encode
 from config import sample_rate
+import sys
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plot
@@ -40,34 +41,52 @@ def draw_wave(signal):
 	plot.plot(time, signal)
 	plot.show()
 
-# The input data, an array of bits
-bits = [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0];
+def get_input_data():
+	# Get the input data, an array of bits from CLI arguments
+	bits = []
+	for i in range(1, len(sys.argv)):
+		if (sys.argv[i].isdigit()):
+			bit = (int)(sys.argv[i])
+			if (bit == 1 or bit == 0):
+				bits.append(bit)
+			else:
+				print("Invalid input data. ABORT")
+				sys.exit()
+	return bits
 
-# Plot the given bits
-draw_bits(bits)
+if __name__ == "__main__":
+	# Get the input data, an array of bits from CLI arguments
+	bits = get_input_data()
+	print("Input data: " + str(bits))
 
-# Convert the input data into an audio signal
-signal = encode(bits)
+	# Plot the given bits
+	draw_bits(bits)
 
-# Draw the generated signal
-draw_wave(signal)
+	# Convert the input data into an audio signal
+	signal = encode(bits)
 
-# Play the audio signal and record it back into @rec
-rec = sd.playrec(signal, sample_rate, channels=2)
+	# Draw the generated signal
+	draw_wave(signal)
 
-# Wait for the audio signal to play out completely
-sd.wait()
+	# Play the audio signal and record it back into @rec
+	rec = sd.playrec(signal, sample_rate, channels=2)
 
-# Draw the recorded signal
-draw_wave(rec)
+	# Wait for the audio signal to play out completely
+	sd.wait()
 
-# Decode the recorded audio signal back into bits
-bits_back = decode(rec)
+	# Draw the recorded signal
+	draw_wave(rec)
 
-# Draw the decoded bit array
-draw_bits(bits_back)
+	# Decode the recorded audio signal back into bits
+	bits_back = decode(rec)
 
-if (bits == bits_back):
-	print("yay")
-else:
-	print("nay")
+	# Draw the decoded bit array
+	draw_bits(bits_back)
+
+	# Print the bit array
+	print("Output data: " + str(bits_back))
+
+	if (bits == bits_back):
+		print("yay")
+	else:
+		print("nay")
