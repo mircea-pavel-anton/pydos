3b522b9ca1dd025d0862d2b6ec0986c29f1e174a
Mircea Anton <mircea.anton99@yahoo.com>
2021-04-19 14:26:25 +0300

    Implement the first draft of the `mod`/`demod` functions

---
+++ src/modulate.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy import signal
+
+from config import sample_rate
+
+def modulate(s):
+	Te = 1 / sample_rate
+	T = s.size * Te # signal duration [s]
+	time = np.arange(0, T, Te)
+
+	# Carrier signal
+	fc = 2500 # carrier signal frequency [Hz]
+	sc = np.cos(2 * np.pi * fc * time) # carrier signal
+
+	# Modulated signal
+	sm = sc * s + sc
+
+	return sm
+
+def demodulate(s):
+	filter_freq = [0, 50, 100, 1000, 1050, 5000] # cutoff frequencies for the filter
+	filter_amp = [0, 1, 0] # The amplitudes for the filter
+	filter_size = 100 # filter size
+
+	filter = signal.remez(filter_size - 1, filter_freq, filter_amp, Hz=sample_rate)
+	return signal.lfilter(filter, 1, np.abs(s))
